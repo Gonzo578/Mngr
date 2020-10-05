5b794c7c992846ddb70bd1f689aa0223c36b7e35
Ralf Hochhausen <micro@ralf-hochhausen.de>
2020-10-05 20:18:28 +0200

    added new list cmd line param

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,16 @@
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <string>
 #include <thread>
 #include <chrono>
 
 using namespace boost::program_options;
 
-void on_age(int age)
-{
-  std::cout << "On age: " << age << '\n';
-}
-
-void on_Name(std::string Name)
-{
-  std::cout << "On name: " << Name << '\n';
+void to_cout(const std::vector<int> &v) {
+  std::copy(v.begin(), v.end(), std::ostream_iterator<int>{
+    std::cout, "\n"});
 }
 
 int main(int argc, const char *argv[])
@@ -24,9 +21,7 @@ int main(int argc, const char *argv[])
     options_description desc{"Options"};
     desc.add_options()
       ("help,h", "Help screen")
-      ("pi", value<float>()->default_value(3.14f), "Pi")
-      ("age", value<int>()->notifier(on_age), "Age")
-	  ("name", value<std::string>()->notifier(on_Name), "Name")
+	  ("nodes,n", value<std::vector<int>>()->multitoken()->composing(), "List of node addresses")
 	  ("tick_s,t", value<int>(&tick)->default_value(1), "Mainloop tick in [s]");
 
     variables_map vm;
@@ -37,12 +32,10 @@ int main(int argc, const char *argv[])
     	std::cout << desc << '\n';
     	return 0;
     }
-    else if (vm.count("name") && vm.count("age"))
-      std::cout << "Hallo " << vm["name"].as<std::string>() << ", wie geht es Dir?" << " Du bis " <<vm["age"].as<int>() << " Jahre alt." << '\n';
-    else if (vm.count("age"))
-      std::cout << "Age: " << vm["age"].as<int>() << '\n';
-    else if (vm.count("pi"))
-      std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
+
+    if(vm.count("nodes")) {
+    	to_cout(vm["nodes"].as<std::vector<int>>());
+    }
 
     while(1) {
     	std::cout << "tick" << '\n';
